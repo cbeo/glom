e0f3d7586bd6b6bee4900b61f5b9fdbd31436869
Colin Okay <okay@toyful.space>
2021-03-01 11:09:03 -0600

    testing error states

--- test/Test.hx
+++ test/Test.hx
@@ -9,17 +9,25 @@ class Test {
   public static function main () {
     var e = new Entity();
     e.set(Comp1, new Comp1("hey",10));
+
+    e.select(Comp1,Comp2)
+      .onOk( (result : {comp1:Comp1,comp2:Comp2}) -> {
+          result.comp1.name = "dude";
+          result.comp2.px = 1000.00;
+        })
+      .onError( err -> trace(err));
+
     e.set(Comp2, new Comp2(22.3, 34.0));
 
-    e.select(Comp1,Comp2).onOk( (result : {comp1:Comp1,comp2:Comp2}) -> {
-        result.comp1.name = "dude";
-        result.comp2.px = 1000.00;
-      });
+    e.select(Comp1,Comp2)
+      .onOk( (result : {comp1:Comp1,comp2:Comp2}) -> {
+          result.comp1.name = "dude";
+          result.comp2.px = 1000.00;
+        })
+      .onError( err -> trace(err));
 
     trace(e.get( Comp1 ));
     trace(e.get( Comp2 ));
-        
-    
 
   }
 }
