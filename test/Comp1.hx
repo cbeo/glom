52d9c0012e8adb84b4adf3e96a9c13e2c3a71f95
Colin Okay <okay@toyful.space>
2021-03-01 13:00:53 -0600

    removed old code

--- test/Comp1.hx
+++
@@ -1,9 +0,0 @@
-package test;
-
-class Comp1 implements glom.Component {
-  public var name:String;
-  public var age:Int;
-
-  public function new (n,a) { this.name = n; this.age = a; }
-
-}
--- test/Comp2.hx
+++
@@ -1,8 +0,0 @@
-package test;
-
-class Comp2 implements glom.Component {
-  public var px:Float;
-  public var py:Float;
-
-  public function new (n,a) { px = n; py = a;}
-}
